c82656272b2a72d04c3cc8d2f7e589539ae21d4c
michael <mchege78@gmail.com>
2020-04-17 23:20:46 +0300

    added a bottom up approach to LMIS (Longest Monotonically Increasing Subsequence)

---
+++ src/com/michael/LongestMonotonicallyIncreasingss/LMIS.java
@@ -0,0 +1,40 @@
+package com.michael.LongestMonotonicallyIncreasingss;
+
+
+
+/**
+ * A java implementation of the Longest Monotonically Increasing Subsequence
+ */
+public class LMIS {
+
+    /**
+     * Bottom up approach
+     * @param x
+     * @return The length of the Longest Monotonically increasing subsequence
+     */
+    public static int lmis(String x){
+
+        int [] dp = new int[x.length()];
+
+        int c = 0;
+
+        for(int i = 0; i < x.length(); i ++){
+            for(int j = i; j >= 0; j --){
+
+                if(x.charAt(i) >= x.charAt(j)){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            c = Math.max(dp[i], c);
+        }
+
+        return c;
+    }
+
+
+    public static void main(String [] a){
+        String x = "AB";
+
+        System.out.println("LMIS " + lmis(x));
+    }
+}
