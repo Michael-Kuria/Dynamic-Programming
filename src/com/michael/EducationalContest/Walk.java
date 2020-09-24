41eb76f6dd75e20f02f2829c6580171229ca9295
michael <mchege78@gmail.com>
2020-09-24 19:27:29 +0300

    added Walk

--- src/com/michael/EducationalContest/Walk.java
+++ src/com/michael/EducationalContest/Walk.java
@@ -1,70 +1,89 @@
 package com.michael.EducationalContest;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Walk {
 
-    public static class Vertex{
-
-        int color = 0;
-        long depth = 0;
-        List<Vertex> adj = new ArrayList<>();
-    }
-
-    static Vertex [] g;
-    static int n, cnt = 0, MOD = (int)(1e7 + 9);
+    static int n, MOD = (int)1e9 + 7;
+    static int [][] g;
     static long k;
 
-    public static void solve(Vertex v){
-
-        if(v.depth == k){
-            cnt ++;
-            cnt %= MOD;
-            return;
-        }
-
-        for(Vertex u: v.adj){
-
-            u.depth = v.depth + 1;
-            solve(u);
-
-
-        }
-
-
+    public static int mul(int a , int b){
+        //long p = a * b;
+        return (int)((long)a * (long)b % MOD);
     }
 
-    public static void main(String [] args){
-        Scanner scan = new Scanner(System.in);
-
-        n = scan.nextInt();
-        k = scan.nextLong();
-
-        g = new Vertex[n];
-
-        for(int i = 0; i < n; i ++){
-            g[i] = new Vertex();
+    public static int add(int a , int b){
+        a += b;
+        if(a >= MOD){
+            a -=  MOD;
         }
+        return a;
+    }
+
+    public static int[][] multiply(int [][] a, int [][] b){
+        int C = a != null? a[0].length: 0;
+        int R = b.length;
+
+        if(R != C)
+            throw new RuntimeException();
+
+        int product[][] = new int[n][n];
 
         for(int i = 0; i < n; i ++){
-            for (int j = 0; j < n; j ++){
-                int x = scan.nextInt();
+            for(int j = 0; j < n; j ++){
+                for(int k = 0; k < n; k ++){
 
-                if(x == 1){
-                    g[i].adj.add(g[j]);
+                    product[i][j] = add(product[i][j], mul(a[i][k], b[k][j]));
                 }
             }
         }
 
+        return product;
+    }
+
+    public static void solve(){
+
+        int [][] result =  new int[n][n];
+
+        // creating an identity matrix
         for(int i = 0; i < n; i ++){
-
-            g[i].depth = 0;
-
-            solve(g[i]);
+            result[i][i] = 1;
         }
 
-        System.out.println(cnt);
+        // matrix exponentiation
+        while(k > 0){
+            if((k & 1) != 0)
+                result = multiply(result, g);
+
+            g = multiply(g, g);
+            k /= 2;
+        }
+
+        int ans  = 0;
+
+        for(int i = 0; i < n; i ++){
+            for(int j = 0; j < n; j ++){
+                ans = add(ans, result[i][j]);
+            }
+        }
+        System.out.println(ans);
     }
+
+    public static void main(String [] args){
+        Scanner scn = new Scanner(System.in);
+        n = scn.nextInt();
+        k = scn.nextLong();
+
+        g = new int[n][n];
+        for(int i = 0; i < n; i ++){
+            for(int j = 0; j < n; j ++){
+                g[i][j] =  scn.nextInt();
+            }
+        }
+
+        solve();
+    }
+
+
 }
